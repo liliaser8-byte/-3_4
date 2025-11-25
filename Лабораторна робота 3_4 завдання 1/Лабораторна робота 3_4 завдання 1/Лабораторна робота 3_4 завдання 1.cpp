ccc5001c1da0b6120d95fc932590b7a3302da890
liliaser8-byte <liliaser8@gmail.com>
2025-11-25 13:29:06 +0200

    Add files via upload

---
+++ Лабораторна робота 3_4 завдання 1/Лабораторна робота 3_4 завдання 1/Лабораторна робота 3_4 завдання 1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <windows.h> 
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int sum = 0;
+    for (int i = 11; i <= 99; i += 2) {
+        sum += i;
+    }
+
+    cout << "Сума всіх двозначних непарних чисел = " << sum << endl;
+    return 0;
+}
---
+++ Лабораторна робота 3_4 завдання 2/Лабораторна робота 3_4 завдання 2/Лабораторна робота 3_4 завдання 2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cmath>
+#include <windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    double a = -4, b = 2;
+    int k;
+    cout << "Введіть k: ";
+    cin >> k;
+
+    double h = (b - a) / k;
+    int i = 0;
+
+    while (i <= k) {
+        double x = a + i * h;
+        double y = exp(3 * x * x + fabs(x + 2));
+        cout << "x = " << x << "\t y = " << y << endl;
+        i++;
+    }
+
+    return 0;
+}
